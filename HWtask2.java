95f8faa830579fa9b9803079f36a230edc3a1f08
legostaev <alexey.legostaev@gmail.com>
2023-02-21 12:45:24 +0300

    the method was rewritten using an iterator

--- HWtask2.java
+++ HWtask2.java
@@ -1,32 +1,33 @@
 package HW;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Random;
 
 public class HWtask2 {
     public static void main(String[] args) {
         // Пусть дан произвольный список целых чисел, удалить из него четные числа
         ArrayList<Integer> newList = getArrayList(10, 0, 10);
-        
+
         System.out.println("исходный список:");
         System.out.println(newList);
 
-        ArrayList<Integer> listWithoutEvenNum = deleteEvenNumbers(newList);
         System.out.println("обработанный список без четных чисел:");
-        System.out.println(listWithoutEvenNum);
-
+        System.out.println(deleteEvenNumbers(newList));
 
     }
 
-    public static ArrayList<Integer> deleteEvenNumbers(ArrayList<Integer> array) {
-        // создает новый массив, удаляя четные числа из исходного
-        ArrayList<Integer> resList = new ArrayList<Integer>(array.size());
+    public static void deleteEvenNumbers(ArrayList<Integer> array) {
+        // удаляет четные числа из списка
 
-        for (int i = 0; i < array.size(); i++) {
-            if (array.get(i) % 2 == 0) continue;
-            else resList.add(array.get(i));
+        for (Iterator<Integer> iterator = array.iterator(); iterator.hasNext();) {
+            Integer number = iterator.next();
+            if (number % 2 == 0) {
+                // System.out.println("This is Even Number: " + number);
+                iterator.remove();
+            }
+
         }
-        return resList;
     }
 
     public static ArrayList<Integer> getArrayList(int size, int min, int max) {
